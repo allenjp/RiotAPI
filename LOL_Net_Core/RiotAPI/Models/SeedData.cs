34982a8a3de47abc28efaf2c3ee93b60134a22d0
allenjp <jeffreypallen3@gmail.com>
2017-04-13 17:00:09 -0400

    Adding start of SeedData class

---
+++ LOL_Net_Core/RiotAPI/Models/SeedData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiotAPI.Models
+{
+    public class SeedData
+    {
+        // see here for info:
+        // https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/working-with-sql
+    }
+}
