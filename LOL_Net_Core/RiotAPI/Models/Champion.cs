ef6abeadc58c0dd310f36538b4e969d4b1136db3
Jeff <jeffreypallen3@gmail.com>
2017-04-13 19:32:24 -0400

    Adding Champion model

---
+++ LOL_Net_Core/RiotAPI/Models/Champion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiotAPI.Models
+{
+    public class Champion
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string PrimeRole { get; set; }
+        public string SecondRole { get; set; }
+    }
+}
