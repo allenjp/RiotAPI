0e1a7b17e8a484605e8666d3f54f5b6ffcd904bb
allenjp <jeffreypallen3@gmail.com>
2017-04-18 13:45:14 -0400

    Adding Match constructor

--- LOL_Net_Core/RiotAPI/Models/Match.cs
+++ LOL_Net_Core/RiotAPI/Models/Match.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,5 +19,33 @@ namespace RiotAPI.Models
         public bool Win { get; set; }
         public string Lane { get; set; }
         public GameType GameType { get; set; }
+
+        public Match(int id, Summoner sum, Champion champ, int kills, int deaths, int assists, int cs, bool win, string lane, GameType gt)
+        {
+            double kda = _calculate_kda(kills, deaths, assists);
+
+            ID = id;
+            Summoner = sum;
+            Champ = champ;
+            Kills = kills;
+            Deaths = deaths;
+            Assists = assists;
+            KDA = kda;
+            CreepScore = cs;
+            Win = win;
+            Lane = lane;
+            GameType = gt;
+        }
+
+        public Match()
+        {
+        }
+
+        private double _calculate_kda(int k, int d, int a)
+        {
+            double result = (k + a) / d;
+
+            return result;
+        }
     }
 }
